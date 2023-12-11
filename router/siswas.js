51ffee8014355ee6eaa4628ae5eaedab9c476085
arbimm <152154392+arbimm@users.noreply.github.com>
2023-12-12 01:41:43 +0700

    Update siswas.js

--- router/siswas.js
+++ router/siswas.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
-const usercontroller = require('../controller/spp')
+const Siswacontroller = require('../controller/siswa')
 
-router.get('/spps', usercontroller.index)
+router.get('/siswas', Siswacontroller.index)
 
-router.get('/spp/:id', usercontroller.show)
+router.get('/siswa/:id', Siswacontroller.show)
 
-router.post('/spp', usercontroller.store)
+router.post('/siswa', Siswacontroller.store)
 
-router.put('/spp/:id', usercontroller.update)
+router.put('/siswa/:id', Siswacontroller.update)
 
-router.delete('/spp/:id', usercontroller.delete)
+router.delete('/siswa/:id', Siswacontroller.delete)
 
 module.exports = router
